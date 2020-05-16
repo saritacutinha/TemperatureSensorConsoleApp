d157585f90057fbcfe26944c0ffe0f534459a710
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-05-16 16:11:04 +0930

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Timers;
+
+namespace TemperatureSensorConsoleApp
+{
+    class Program
+    {
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            
+            Timer timer = new Timer(60000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+            Console.ReadKey();
+            timer.Stop();
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var currentTemperature = random.Next(-4, 45);
+            Console.WriteLine("DateTime :"+DateTime.Now +"\tTemperature :"+currentTemperature );
+        }
+    }
+}
